3e6f711c9d3bc9d8f0776a9de2b71276f41a4c32
Henry <junean66@gmail.com>
2026-07-12 15:19:34 +0800

    initial commit

---
+++ packages/ui/src/components/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@full-stack-learning/ui/lib/utils";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-none bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
